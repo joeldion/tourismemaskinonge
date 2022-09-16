9c2f0c2d17b3eed220b3a3e0f8d173b03ab8152f
joeldion <info@joeldion.com>
2022-09-16 14:24:29 -0400

    Added footer newsletter links (editable)

--- inc/settings/footer/footer-newsletter.php
+++ inc/settings/footer/footer-newsletter.php
@@ -31,6 +31,30 @@ add_settings_field(
 );
 register_setting( 'footer-settings', 'footer_newsletter_desc' );
 
+// Links (2)
+for ($i = 1; $i <= 2; $i++) {
+
+    add_settings_field(
+        'footer_newsletter_link_label_' . $i,
+        esc_html__( 'Link Title', TM_DOMAIN ) . ' ' . $i,
+        'footer_newsletter_link_label_markup',
+        'footer-settings-page',
+        'footer-newsletter-settings-section',
+        [ 'index' => $i ]
+    );
+    register_setting( 'footer-settings', 'footer_newsletter_link_label_' . $i );
+
+    add_settings_field(
+        'footer_newsletter_link_url_' . $i,
+        esc_html__( 'Link URL', TM_DOMAIN ) . ' ' . $i,
+        'footer_newsletter_link_url_markup',
+        'footer-settings-page',
+        'footer-newsletter-settings-section',
+        [ 'index' => $i ]
+    );
+    register_setting( 'footer-settings', 'footer_newsletter_link_url_' . $i );
+}
+
 function footer_newsletter_settings_section_callback() {}
 
 function footer_newsletter_title_markup() {
@@ -45,6 +69,22 @@ function footer_newsletter_desc_markup() {
 
     ?>
         <textarea name="footer_newsletter_desc" cols="80" rows="5"><?php esc_html_e( get_option( 'footer_newsletter_desc' ) ); ?></textarea>
+    <?php
+
+}
+
+function footer_newsletter_link_label_markup( $args ) {
+
+    ?>  
+        <input type="text" size="60" maxlength="100" name="footer_newsletter_link_label_<?php echo $args['index']; ?>" value="<?php esc_html_e( get_option( 'footer_newsletter_link_label_' . $args['index'] ) ); ?>">
+    <?php
+
+}
+
+function footer_newsletter_link_url_markup( $args ) {
+
+    ?>  
+        <input type="url" size="60" maxlength="300" name="footer_newsletter_link_url_<?php echo $args['index']; ?>" value="<?php esc_html_e( get_option( 'footer_newsletter_link_url_' . $args['index'] ) ); ?>">
     <?php
 
 }
