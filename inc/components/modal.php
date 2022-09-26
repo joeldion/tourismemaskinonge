65bb888357bae270b7102e8b1c0fa6cc05c2e6ac
joeldion <info@joeldion.com>
2022-09-26 10:52:47 -0400

    Added style for single column in modal

--- inc/components/modal.php
+++ inc/components/modal.php
@@ -12,19 +12,22 @@ function tm_get_modal() {
     <div class="tm-modal">
         <div class="tm-modal__close" id="modal-close"></div>
         <div class="tm-modal__header">
-            <h3 class="tm-modal__title">Portes ouvertes chez les producteurs, artistes et artisans</h3> 
-            <h4 class="tm-modal__subtitle">Plus de 40 arrêts, partout dans la MRC de Maskinongé</h4>
-            <h5 class="tm-modal__title">24 et 25 septembre 2022<br /> 10&nbsp;h à 17&nbsp;h</h5>
+            <h3 class="tm-modal__title">Bon appétit Maski</h3> 
+            <h4 class="tm-modal__subtitle">Événement gourmand en restaurant</h4>
+            <h5 class="tm-modal__title">Du 20 au 30 octobre 2022</h5>
         </div>        
-        <div class="tm-modal__content">
-            <div class="tm-modal__item">
-                <img class="tm-modal__img" src="<?php echo TM_URL . '/img/logos/logo-tournee-bon-appetit-maski-modal-1x.png'; ?>" srcset="<?php echo TM_URL . '/img/logos/logo-tournee-bon-appetit-maski-modal-1x.png 1x, ' . TM_URL . '/img/logos/logo-tournee-bon-appetit-maski-modal.png 2x'; ?>" alt="Bon appétit Maski">
+        <div class="tm-modal__content tm-modal__content--single">
+            <div class="tm-modal__item tm-modal__item--single">
+                <img class="tm-modal__img" src="<?php echo TM_URL . '/img/logos/logo-bon-appetit-maski-modal-resto-1x.png'; ?>" srcset="<?php echo TM_URL . '/img/logos/logo-bon-appetit-maski-modal-resto-1x.png 1x, ' . TM_URL . '/img/logos/logo-bon-appetit-maski-modal-resto.png 2x'; ?>" alt="Bon appétit Maski" height="235" width="300">
                 <div class="tm-modal__desc">
-                    <h4 class="tm-modal__title tm-modal__title--secondary">Tournée gourmande</h4>
-                    <p>Dégustez et achetez frais directement de la ferme. Découvrez le savoir-faire de nos producteurs agroalimentaires.</p>
-                    <p><a href="https://bonappetitmaski.com" class="tm-modal__cta" target="_blank" rel="noopener" aria-label="Bon appétit Maski">En savoir plus</a></p>                
+                    <?php /* <h4 class="tm-modal__title tm-modal__title--secondary">Tournée gourmande</h4> */ ?>
+                    <p>Produits locaux en vedette dans des tables d’hôte trois services au prix unique de 45$. </p>
+                    <p>Certains restaurateurs offrent la table d’hôte pour emporter. </p>
+                    <p>Réservez votre repas auprès des restaurateurs participants.</p>
+                    <p><a href="https://bonappetitmaski.com/#restaurants" class="tm-modal__cta" target="_blank" rel="noopener" aria-label="Bon appétit Maski">En savoir plus</a></p>                
                 </div>
             </div>
+            <?php /*
             <div class="tm-modal__item">
                 <img class="tm-modal__img" src="<?php echo TM_URL . '/img/logos/logo-hangarts-publics-modal-1x.png'; ?>" srcset="<?php echo TM_URL . '/img/logos/logo-hangarts-publics-modal-1x.png 1x, ' . TM_URL . '/img/logos/logo-hangarts-publics-modal.png 2x'; ?>" alt="HangARTS publics">
                 <div class="tm-modal__desc">
@@ -41,6 +44,7 @@ function tm_get_modal() {
                     <p><a href="https://www.escapademauricie.com/fr-ca/arts-et-tournee-gourmande-maskinonge" class="tm-modal__cta" target="_blank" rel="noopener" aria-label="Escapade Mauricie">En savoir plus</a></p>                
                 </div>
             </div>
+            */ ?>
         </div>
     </div>
     <?php
