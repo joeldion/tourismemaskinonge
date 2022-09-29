a9c0d550a905a64036aae4943344244039b69d5a
joeldion <info@joeldion.com>
2022-09-29 14:07:44 -0400

    Added redirection for drafts + localization

--- 404.php
+++ 404.php
@@ -3,7 +3,6 @@
  * 404
 */
 ?>
-
 <main id="post-<?php the_ID(); ?>" <?php post_class('main tm-404'); ?>>
 
     <?php  echo get_slider(); ?>
@@ -11,12 +10,12 @@
     <section class="tm-404__title section">
         <div class="text-block">
             <h2 class="text-block__small text-block__small--yellow-l">404</h2>
-            <h1 class="text-block__big text-block__big--yellow-d">Contenu introuvable</h1>
-            <p class="text-block__body">Nous sommes désolés. Aucun contenu n'a été trouvé.</p>
-            <p class="text-block__body">Vous pouvez essayer le formulaire de recherche suivant :</p>
+            <h1 class="text-block__big text-block__big--yellow-d"><?php esc_html_e( 'Content not found', TM_DOMAIN ); ?></h1>
+            <p class="text-block__body"><?php esc_html_e( "We're sorry. No content was found.", TM_DOMAIN ); ?></p>
+            <p class="text-block__body"><?php esc_html_e( 'You may try the following search form:', TM_DOMAIN ); ?></p>
             <form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="post" class="tm-search-small__form tm-search-small__form--404">
                 <input type="submit" class="tm-search-small__form-submit">
-                <input type="text" class="tm-search-small__input" name="s" placeholder="Rechercher">
+                <input type="text" class="tm-search-small__input" name="s" placeholder="<?php echo esc_html_x( 'Search', 'Search verb', TM_DOMAIN ); ?>">
             </form>
         </div>        
     </section>
